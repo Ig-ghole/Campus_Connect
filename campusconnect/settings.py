06fc9699c4b0f4f74f0444596df61699001c80bf
Ig-ghole <accernicerr@gmail.com>
2026-07-03 21:37:43 +0545

    final

--- campusconnect/settings.py
+++ campusconnect/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('campusconnect2006@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('zkkn bels ptzo urdu')
 DEFAULT_FROM_EMAIL = 'noreply@nec.edu.np' 
 
 EMAIL_TIMEOUT = 10
